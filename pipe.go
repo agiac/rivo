7e0d51046ebada5251707ffd4488126533758b4c
Alberto Giachino <alberto.giachino@limango.com>
2025-01-09 18:59:47 +0100

    refactor pipe tests

--- pipe.go
+++ pipe.go
@@ -2,38 +2,34 @@ package rivo
 
 import "context"
 
-type None struct{}
-
-// Pipeline is a function that takes a context and a stream and returns a stream. It is the building block of a data pipeline.
-type Pipeline[T, U any] func(ctx context.Context, stream Stream[T]) Stream[U]
-
-// Pipe pipes two pipeable functions together. It is a convenience function that calls Pipe2.
+// Pipe pipes two pipelines together.
+// It is a convenience function that calls Pipe2.
 func Pipe[A, B, C any](a Pipeline[A, B], b Pipeline[B, C]) Pipeline[A, C] {
 	return Pipe2(a, b)
 }
 
-// Pipe2 pipes two pipeable functions together.
+// Pipe2 pipes two pipelines together.
 func Pipe2[A, B, C any](a Pipeline[A, B], b Pipeline[B, C]) Pipeline[A, C] {
 	return func(ctx context.Context, stream Stream[A]) Stream[C] {
 		return b(context.WithoutCancel(ctx), a(ctx, stream))
 	}
 }
 
-// Pipe3 pipes three pipeable functions together.
+// Pipe3 pipes three pipelines together.
 func Pipe3[A, B, C, D any](a Pipeline[A, B], b Pipeline[B, C], c Pipeline[C, D]) Pipeline[A, D] {
 	return func(ctx context.Context, stream Stream[A]) Stream[D] {
 		return c(context.WithoutCancel(ctx), b(context.WithoutCancel(ctx), a(ctx, stream)))
 	}
 }
 
-// Pipe4 pipes four pipeable functions together.
+// Pipe4 pipes four pipelines together.
 func Pipe4[A, B, C, D, E any](a Pipeline[A, B], b Pipeline[B, C], c Pipeline[C, D], d Pipeline[D, E]) Pipeline[A, E] {
 	return func(ctx context.Context, stream Stream[A]) Stream[E] {
 		return d(context.WithoutCancel(ctx), c(context.WithoutCancel(ctx), b(context.WithoutCancel(ctx), a(ctx, stream))))
 	}
 }
 
-// Pipe5 pipes five pipeable functions together.
+// Pipe5 pipes five pipelines together.
 func Pipe5[A, B, C, D, E, F any](a Pipeline[A, B], b Pipeline[B, C], c Pipeline[C, D], d Pipeline[D, E], e Pipeline[E, F]) Pipeline[A, F] {
 	return func(ctx context.Context, stream Stream[A]) Stream[F] {
 		return e(context.WithoutCancel(ctx), d(context.WithoutCancel(ctx), c(context.WithoutCancel(ctx), b(context.WithoutCancel(ctx), a(ctx, stream)))))
--- stream.go
+++ stream.go
@@ -1,5 +1,12 @@
 package rivo
 
+import "context"
+
+// None is a type that represents no value.
+// It is typically used as the input type of generator pipeline that does not
+// depend on any input stream or for a sync pipeline that does not emit any items.
+type None struct{}
+
 // Item represents a single item in a data stream. It contains a value of type T and an optional error.
 type Item[T any] struct {
 	// Val is the value of the item when there is no error.
@@ -10,3 +17,6 @@ type Item[T any] struct {
 
 // Stream represents a data stream of items. It is a read only channel of Item[T].
 type Stream[T any] <-chan Item[T]
+
+// Pipeline is a function that takes a context and a stream and returns a stream of the same type or a different type.
+type Pipeline[T, U any] func(ctx context.Context, stream Stream[T]) Stream[U]
