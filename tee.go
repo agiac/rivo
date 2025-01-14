e0d031ffeb213abc1427269d9e9595f909d7bf83
Alberto Giachino <alberto.giachino@limango.com>
2025-01-14 15:11:47 +0100

    setup new tee

--- tee.go
+++ tee.go
@@ -4,12 +4,6 @@ import (
 	"context"
 )
 
-// Tee returns two streams that each receive a copy of each item from the input stream. It is equivalent to TeeN(ctx, in, 2).
-func Tee[T any](ctx context.Context, in Stream[T]) (Stream[T], Stream[T]) {
-	out := TeeN(ctx, in, 2)
-	return out[0], out[1]
-}
-
 // TeeN returns n streams that each receive a copy of each item from the input stream.
 func TeeN[T any](ctx context.Context, in Stream[T], n int) []Stream[T] {
 	if n <= 1 {
@@ -41,4 +35,52 @@ func TeeN[T any](ctx context.Context, in Stream[T], n int) []Stream[T] {
 	}
 
 	return streams
+}
+
+func Tee2[T, U any](p Pipeline[T, U]) func(context.Context, Stream[T]) (Pipeline[None, U], Pipeline[None, U]) {
+	return func(ctx context.Context, s Stream[T]) (Pipeline[None, U], Pipeline[None, U]) {
+		res := Tee2N[T, U](p, 2)(ctx, s)
+		return res[0], res[1]
+	}
+}
+
+func Tee2N[T, U any](p Pipeline[T, U], n int) func(context.Context, Stream[T]) []Pipeline[None, U] {
+	if n <= 1 {
+		panic("n must be greater than 1")
+	}
+
+	return func(ctx context.Context, s Stream[T]) []Pipeline[None, U] {
+		out := make([]chan Item[U], n)
+		for i := 0; i < n; i++ {
+			out[i] = make(chan Item[U])
+		}
+
+		go func() {
+			defer func() {
+				for i := 0; i < n; i++ {
+					close(out[i])
+				}
+			}()
+
+			for item := range OrDone(ctx, p(ctx, s)) {
+				for i := 0; i < n; i++ {
+					select {
+					case out[i] <- item:
+					case <-ctx.Done():
+						out[i] <- Item[U]{Err: ctx.Err()}
+					}
+				}
+			}
+		}()
+
+		pipes := make([]Pipeline[None, U], n)
+		for i := 0; i < n; i++ {
+			out := out[i]
+			pipes[i] = func(ctx context.Context, _ Stream[None]) Stream[U] {
+				return out
+			}
+		}
+
+		return pipes
+	}
 }
