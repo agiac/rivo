df502d2db5c19fd34991ba2a0005d5c8ec4683c4
Alberto Giachino <alberto.giachino@limango.com>
2025-01-20 20:24:43 +0100

    setup ForEach2

--- forEach_test.go
+++ forEach_test.go
@@ -3,6 +3,7 @@ package rivo_test
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"testing"
 
 	. "github.com/agiac/rivo"
@@ -33,6 +34,43 @@ func ExampleForEach() {
 
 	// Output:
 	// item: {}; error: an error
+}
+
+func ExampleForEach2() {
+	ctx := context.Background()
+
+	g := Of("1", "!2", "w", "4", "5")
+
+	errHandler := Do[struct{}](func(ctx context.Context, i Item[struct{}]) {
+		fmt.Printf("Error\n")
+	})
+
+	res := make([]int, 0)
+	forEachFn := func(ctx context.Context, i Item[string]) error {
+		n, err := strconv.Atoi(i.Val)
+		if err != nil {
+			return err
+		}
+
+		res = append(res, n)
+
+		return nil
+	}
+
+	f := ForEach2[string](forEachFn, errHandler)
+
+	<-Pipe(g, f)(ctx, nil)
+
+	for _, n := range res {
+		fmt.Printf("%v\n", n)
+	}
+
+	// Output:
+	// Error
+	// Error
+	// 1
+	// 4
+	// 5
 }
 
 func TestForEach(t *testing.T) {
